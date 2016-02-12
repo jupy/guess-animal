df320bac3935879221ef7158092a02634a1cb6a0
Mikhail Navernyuk <mike.navern@gmail.com>
2016-02-12 10:32:56 +0300

    nodes route added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./controllers/index');
 var users = require('./controllers/users');
+var nodes = require('./controllers/nodes');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/nodes', nodes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
