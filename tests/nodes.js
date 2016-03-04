87618942fdaaa55460d82c59d3afa7d6ab5987f4
Mikhail Navernyuk <mike.navern@gmail.com>
2016-03-04 15:16:25 +0300

    tests improved

--- tests/nodes.js
+++ tests/nodes.js
@@ -1,7 +1,17 @@
-var app = require('../app'),
-  assert = require('assert'),
-  request = require('superagent');
+var app = require('../app');
+var assert = require('assert');
+var request = require('superagent');
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+describe('Proba', function() {
+  it('should be ok for the first test', function() {
+    assert.equal(true, true);
+  });
+
+  it('should be NOT ok for the second test', function() {
+    assert.equal(true, false);
+  });
 });
